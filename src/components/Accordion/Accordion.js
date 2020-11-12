fc2f3dfe7798bb895fe4a97b4ebfa895e685114a
Brent Ahrens <cidida@gmail.com>
2020-11-12 12:01:38 -0800

    sets up initial structure

---
+++ src/components/Accordion/Accordion.js
@@ -0,0 +1,11 @@
+import '../../scss/_accordion.scss'
+
+const Accordion = () => {
+    return (
+        <div className="accordion">
+            <h1>Accordion</h1>
+        </div>
+    )
+}
+
+export default Accordion
---
+++ src/components/Resume/Resume.js
@@ -0,0 +1,14 @@
+import '../../scss/_resume.scss'
+
+import Accordion from '../Accordion/Accordion'
+
+const Resume = () => {
+    return (
+        <div className="resume">
+            <h1>Resume</h1>
+            <Accordion />
+        </div>
+    )
+}
+
+export default Resume
